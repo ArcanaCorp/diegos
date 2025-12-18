0ad8c34ac3afb72cdc48e020211008c9c7fec0ec
Jhans-15 <72289225+JFrancodev12@users.noreply.github.com>
2025-12-18 14:29:45 -0500

    fix:subscribePush

--- src/featured/dashboard/tabs/config/page.js
+++ src/featured/dashboard/tabs/config/page.js
@@ -1,10 +1,12 @@
 import { toast } from "sonner"
 import { enablePush } from "@/services/settings.service"
+import { requestNotificationPermission } from "@/helpers/requestNotificationPermission";
 
 export default function ConfigPage () {
 
     const handleActiveNotifications = async () => {
         try {
+            await requestNotificationPermission();
             await enablePush();
         } catch (error) {
             toast.error('Error', { description: `${error.message}` })
